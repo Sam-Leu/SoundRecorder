c99c7e0951af78ca18fe91e398b740a5ca6c427b
one <1161372240@qq.com>
2018-12-07 11:54:41 +0800

    获取权限

--- app/src/main/java/com/example/one/soundrecorder/MainActivity.java
+++ app/src/main/java/com/example/one/soundrecorder/MainActivity.java
@@ -30,7 +30,7 @@ import java.util.Date;
 import java.util.List;
 
 //实现ActivityCompat.OnRequestPermissionsResultCallback接口，运行时检测权限
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+public class MainActivity extends AppCompatActivity {
 
     private DatabaseHelper dbHelper;
     private SQLiteDatabase db;
@@ -45,105 +45,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private File soundsFolder = null;
     private File soundFile = null;
     private Date createTime = null;
-
-
-    //需要进行检测的权限数组
-    protected String[] needPermissions = {
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,//存储卡写入权限
-            Manifest.permission.READ_EXTERNAL_STORAGE,//存储卡读取权限
-            Manifest.permission.RECORD_AUDIO,   //录音权限
-            Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,  //允许装载和卸载文件系统权限，这个一定要加，否则手机即使赋予了权限也不会记录下来
-            Manifest.permission.READ_PHONE_STATE//读取手机状态权限
-    };
-    private static final int PERMISSON_REQUESTCODE = 0;
-    //判断是否需要检测，防止不停的弹框
-    private boolean isNeedCheck = true;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (isNeedCheck) {
-            checkPermissions(needPermissions);
-        }
-    }
-    //检查权限
-    private void checkPermissions(String... permissions) { List<String> needRequestPermissonList = findDeniedPermissions(permissions);
-        if (null != needRequestPermissonList
-                && needRequestPermissonList.size() > 0) {
-            ActivityCompat.requestPermissions(this,
-                    needRequestPermissonList.toArray(
-                            new String[needRequestPermissonList.size()]),
-                    PERMISSON_REQUESTCODE);
-        }
-    }
-    //获取权限集中需要申请权限的列表
-    private List<String> findDeniedPermissions(String[] permissions) {
-        List<String> needRequestPermissonList = new ArrayList<String>();
-        for (String perm : permissions) {
-            if (ContextCompat.checkSelfPermission(this,
-                    perm) != PackageManager.PERMISSION_GRANTED
-                    || ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, perm)) {
-                needRequestPermissonList.add(perm);
-            }
-        }
-        return needRequestPermissonList;
-    }
-    //检测是否有的权限都已经授权
-    private boolean verifyPermissions(int[] grantResults) {
-        for (int result : grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] paramArrayOfInt) {
-        if (requestCode == PERMISSON_REQUESTCODE) {
-            if (!verifyPermissions(paramArrayOfInt)) {
-                showMissingPermissionDialog();
-                isNeedCheck = false;
-            }
-        }
-    }
-    //弹出对话框, 提示用户手动授权
-    private void showMissingPermissionDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.notifyTitle);
-        builder.setMessage(R.string.notifyMsg);
-        // 拒绝授权 退出应用
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-        //同意授权
-        builder.setPositiveButton(R.string.setting, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startAppSettings();
-            }
-        });
-        builder.setCancelable(false);
-        builder.show();
-    }
-    //启动应用的设置
-    private void startAppSettings() {
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.parse("package:" + getPackageName()));
-        startActivity(intent);
-    }
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            this.finish();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-    //以上是动态获取权限的方法**********************************************************************
 
 
     @SuppressLint("SimpleDateFormat")
